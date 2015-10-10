81981d05c70c50e095e04c3d9f19c2297411bbc6
Andrey Korotkov <goodlak13@bk.ru>
2015-10-10 14:30:35 +0300

    Less7

--- src/Box.java
+++ src/Box.java
@@ -5,4 +5,28 @@ public class Box {
     double width;
     double heigth;
     double depth;
+
+    void volume(){
+        System.out.print("Volume is ");
+        System.out.println(width*heigth*depth);
+    }
+    double volume1(){
+        return width*heigth*depth;
+    }
+
+    Box(){//конструктор по умолчанию
+        System.out.println("Constructing Box");
+        width = 10;
+        heigth = 10;
+        depth = 10;
+    }
+    Box(double w, double h, double d){//конструктор по умолчанию
+        System.out.println("Constructing Box");
+        width = w;
+        heigth = h;
+        depth = d;
+    }
+    protected void finalize(){
+        System.out.println("Goodbye Box");
+    }
 }
